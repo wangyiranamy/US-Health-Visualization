569d14992e3cc70daddf652376d05b37f6637ed8
wangyiranamy <yw3201@columbia.edu>
2018-11-29 16:20:02 -0500

    shiny

--- basic_visualization_4/US_Health/app.R
+++ basic_visualization_4/US_Health/app.R
@@ -9,69 +9,70 @@
 
 library(shiny)
 library(plotly)
-summary_measure_state <- read.csv("summary_measure_state.csv")
+library(leaflet)
+
+summary_measure_state <- read.csv("/Users/yiranwang/US-Health-Visualization/basic_visualization_4/US_Health/summary_measure_state.csv")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-  titlePanel("US Health Status"),
-  plotlyOutput("plot"),
-  verbatimTextOutput("click"),
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Create demographic maps with 
-               information from the US Health Status Summary."),
-      
-      selectInput("var", 
-                  label = "Choose a variable to display",
-                  choices = c("Average Life Expectancy", "Unhealthy Days",
-                              "Health Status", "All Death"),
-                  selected = "Average Life Expectancy")
-      
-      # sliderInput("range", 
-      #             label = "Range of interest:",
-      #             min = 0, max = 100, value = c(0, 100))
-    ),
-    
-    mainPanel(plotOutput("map"))
-  )
+ui <- navbarPage("US Health Status", id="ush",
+      tabPanel("US Health Summary",
+               sidebarLayout(
+                 sidebarPanel(
+                   helpText("Create demographic maps with 
+                information from the US Health Status Summary."),
+                selectInput(inputId = "var", 
+                           label = "Choose a variable to display", 
+                           choices = c("Average Life Expectancy", "Unhealthy Days",
+                                       "Health Status", "All Death"), 
+                           selected = "Average Life Expectancy")),
+                mainPanel(
+                  plotlyOutput("map"))
+                )
+                
+      )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, var, session) {
-  
-  output$plot <- renderPlotly({
+  output$map <- renderLeaflet({
     data <- switch(input$var, 
                    "Average Life Expectancy" = summary_measure_state$ALE,
                    "Unhealthy Days" = summary_measure_state$Unhealthy_Days,
-                   "Healthy Status" = summary_measure_state$Healthy_Status,
+                   "Healthy Status" = summary_measure_state$Health_Status,
                    "All Death" = summary_measure_state$All_Death)
+    summary_measure_state$hover <- with(summary_measure_state, paste(State_Name, '<br>', "ALE", ALE, "Unhealthy_Days", Unhealthy_Days, "<br>",
+                               "Healthy_Status", Health_Status))
     l <- list(color = toRGB("white"), width = 2)
     g <- list(
       scope = 'usa',
+      showcountries = TRUE,
       projection = list(type = 'albers usa'),
       showlakes = TRUE,
       lakecolor = toRGB('white')
     )
     plot_geo(summary_measure_state, locationmode = 'USA-states') %>%
       add_trace(
-        z = ~data, text = ~data, locations = ~CHSI_State_Abbr,
-        color = ~data, colors = 'Purples'
+        z = ~data, text = ~hover, locations = ~CHSI_State_Abbr,
+        color = ~data, colors = 'Oranges'
       ) %>%
       colorbar(title = input$var) %>%
       layout(
-        title = '2011 US Average Life Expectation by State<br>(Hover for breakdown)',
+        title = 'US Health Summary by State',
         geo = g
       )
-    # plot_ly(z = state.area, text = state.name, locations = state.abb,
-    #         type = 'choropleth', locationmode = 'USA-states') %>%
-    #   layout(geo = g)
   })
-  
-  # output$click <- renderPrint({
-  #   d <- event_data("plotly_click")
-  #   if (is.null(d)) "Click on a state to view event data" else d
+ 
+  # output$bar <- renderPlotly({
+  #   var <- switch(input$var, 
+  #                  "Average Life Expectancy" = summary_measure_state$ALE,
+  #                  "Unhealthy Days" = summary_measure_state$Unhealthy_Days,
+  #                  "Healthy Status" = summary_measure_state$Health_Status,
+  #                  "All Death" = summary_measure_state$All_Death)
+  #   p <- ggplot(summary_measure_state, aes(CHSI_State_Abbr,var))+
+  #     geom_bar(stat = "identity")+
+  #     theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  #   ggplotly(p)
   # })
-  
 }
 
 # Run the application 
